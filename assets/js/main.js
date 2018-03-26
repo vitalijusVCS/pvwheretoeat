2518f7f7a91a2c0c01430ddf1b23044cbed753a2
PawellM86 <37325576+PawellM86@users.noreply.github.com>
2018-03-26 17:49:23 +0300

    topbutton

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,28 @@
-$( document ).ready(function() {
+$( document ).ready(function($) {
 	$( "#vshowmore" ).click(function() {
 		$( "#ggg" ).show( "slow" );
 		$( "#vshowmore" ).hide( "slow" );
 		$( "#vhide" ).show( "slow" );
-	}); 
+	});
 	$( "#vhide" ).click(function() {
   		$( "#ggg" ).hide( 1000 );
   		$( "#vhide" ).hide( "slow" );
 		$( "#vshowmore" ).show( "slow" );
 	});
+
+
+// scrolling button
+	$("#scrolling").click(function () {
+				$('html, body').animate({
+						scrollTop:$(".vsearchcontainer").offset().top
+				}, 1000);
+		 });
+	window.onscroll = function() {scrollFunction()};
+	 function scrollFunction() {
+			 if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+					 document.getElementById("scrolling").style.display = "block";
+			 } else {
+					 document.getElementById("scrolling").style.display = "none";
+			 }
+	 }
 });
